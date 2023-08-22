2e3312c0b3588d6e3ac6153a3a786c5d33a83ae5
Catherine <70590533+catherinetkl@users.noreply.github.com>
2023-08-23 01:58:40 +0800

    Update Exercise3point5.java

--- programming-basics/ood1/methods/Exercise3point5.java
+++ programming-basics/ood1/methods/Exercise3point5.java
@@ -1,3 +1,13 @@
+/* Write a method called extractEvenArray. 
+It should take an int array as an argument. 
+It should return an int array which contains all of the even numbers from the original array. 
+The returned array should not be any bigger than the size required to hold the even numbers. 
+The even numbers in the returned array should be in the same order as in the original array. 
+For example:
+With this array as an argument: 	[2,5,3,7,6,7,3,4,3]
+
+The method would return:		[2,6,4] */
+
 package com.fdmgroup.exercise5_part3;
 
 public class Exercise3point5 {
